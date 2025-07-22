9031ad9505f13044d405d845b04ad858c3a96646
trendingeswari <trendingeswari@gmail.com>
2025-07-22 10:27:19 +0530

    Core Java

---
+++ C2TC_Core_Java/src/Day3/Encapsulation/EncapsulationExample.java
@@ -0,0 +1,14 @@
+package Day3.Encapsulation;
+
+public class EncapsulationExample {
+public static void main(String[] args) {
+		
+		//object creation 
+		OopsConceptDemo obj = new OopsConceptDemo();
+		obj.setSerialNum(101);
+		obj.setName("Pooja");
+		obj.setAge(20);
+		System.out.println(obj);
+		
+	}
+}
